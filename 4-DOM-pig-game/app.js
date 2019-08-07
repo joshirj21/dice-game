a79f156e4612c06d46065f68b27bc053561b6a61
Rahul joshi <joshirj@android-2120ee3b45.localdomain>
2019-08-07 13:58:35 +0530

    initial commit

---
+++ 4-DOM-pig-game/app.js
@@ -0,0 +1,74 @@
+/*
+GAME RULES:
+
+- The game has 2 players, playing in rounds
+- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
+- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
+- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+- The first player to reach 100 points on GLOBAL score wins the game
+
+*/
+
+var score = 0;
+var scores = [];
+scores[0] = 0;
+scores[1] = 0;
+var activeplayer = 0;
+
+document.querySelector("#current-" + 0).textContent = 0
+document.querySelector("#current-" + 1).textContent = 0
+
+document.querySelector("#score-0").textContent = 0;
+document.querySelector("#score-1").textContent = 0;
+function random() {
+    return Math.floor(Math.random() * 6 + 1);
+}
+
+
+
+// document.querySelector(".btn-roll").addEventListener("click", function () {
+//     score = random();
+//     if (score !== 1) {
+//         score[activeplayer] = score[activeplayer] + score;
+//         document.querySelector("#current-" + activeplayer).textContent = score[activeplayer];
+//     }
+//     else {
+//         score[activeplayer] = 0;
+//         document.querySelector("#current-" + activeplayer).textContent = score[activeplayer];
+//     }
+
+// })
+
+document.querySelector(".btn-hold").addEventListener("click", function () {
+    document.querySelector("#score-" + activeplayer).textContent = scores[activeplayer];
+    document.querySelector("#current-" + 0).textContent = 0;
+    document.querySelector("#current-" + 1).textContent = 0;
+    if (activeplayer === 0) {
+        activeplayer = 1;
+    }
+    else if (activeplayer === 1) {
+        activeplayer = 0;
+    }
+})
+
+document.querySelector(".btn-roll").addEventListener("click", function () {
+    score = random();
+    document.getElementsByClassName("dice")[0].setAttribute("src", "../dice-" + score + ".png")
+    if (score !== 1) {
+        scores[activeplayer] = scores[activeplayer] + score;
+        document.querySelector("#current-" + activeplayer).textContent = Number(document.querySelector("#current-" + activeplayer).textContent) + score;
+    }
+    else {
+        scores[activeplayer] = 0;
+        document.querySelector("#score-" + activeplayer).textContent = scores[activeplayer];
+        document.querySelector("#current-" + 0).textContent = 0;
+        document.querySelector("#current-" + 1).textContent = 0;
+        if (activeplayer === 0) {
+            activeplayer = 1;
+        }
+        else if (activeplayer === 1) {
+            activeplayer = 0;
+        }
+    }
+
+})
